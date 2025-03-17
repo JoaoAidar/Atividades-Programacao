3a7d4199a762ae5b4024ed518a68f0d6cf6a1932
CostelasBode <joao.aidar@sou.inteli.edu.br>
2025-03-17 20:41:16 -0300

    Update Scene_Level02.js

--- Aula_Sem06/scenes/Scene_Level02.js
+++ Aula_Sem06/scenes/Scene_Level02.js
@@ -8,22 +8,42 @@ export class Scene_Level02 extends Phaser.Scene {
         this.load.image('backgroundGame2', 'assets/background_game2.png');
         this.load.image('tiles', 'assets/tileset.png');
         this.load.image('player', 'assets/player.png');
+        this.load.image('enemy', 'assets/enemy.png');
+        this.load.image('door', 'assets/door.png');
     }
 
     create() {
         this.add.image(400, 300, 'backgroundGame2');
-        
+
+
         
         // Add player
         this.player = this.physics.add.sprite(320, 320, 'player');
         this.player.setCollideWorldBounds(true);
 
 
+
+        // Add enemy
+        this.enemy = this.physics.add.sprite(500, 300, 'enemy');
+        this.enemy.setVelocity(100, 100);
+        this.enemy.setBounce(1, 1);
+        this.enemy.setCollideWorldBounds(true);
+
+
+        // Add door
+        this.door = this.physics.add.sprite(300, 100, 'door');
+        this.physics.add.overlap(this.player, this.door, this.enterDoor, null, this);
+
         // Add text
         this.add.text(100, 100, "Fase 2 - Novo Desafio", { fontSize: "32px", fill: "#fff" });
 
         // Input keys
         this.cursors = this.input.keyboard.createCursorKeys();
+
+        // Collision with enemy
+        this.physics.add.overlap(this.player, this.enemy, () => {
+            this.scene.start('GameOver_Scene');
+        });
     }
 
     update() {
@@ -40,6 +60,10 @@ export class Scene_Level02 extends Phaser.Scene {
         } else if (this.cursors.down.isDown) {
             this.player.setVelocityY(160);
         }
+    }
+
+    enterDoor(player, door) {
+        this.scene.start('WinScene');
     }
 }
 
